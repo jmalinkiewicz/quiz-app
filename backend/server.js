45a9553eb5ee56bab1208776a5d3e1438b42aa2d
Jakub Malinkiewicz <me@jmalinkiewicz.pl>
2024-07-11 22:51:01 +0200

    init prisma ORM and connect to the DB

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,41 @@
 const express = require("express");
 const cors = require("cors");
+const { PrismaClient } = require("@prisma/client");
 
 const app = express();
 const port = 8000;
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    next();
-  });
+const prisma = new PrismaClient();
 
-  app.use(
-    cors({
-      origin: "http://localhost:3000", // Be cautious in production, use specific domains instead of '*'
-      methods: ["GET", "POST", "PUT", "DELETE"],
-      allowedHeaders: ["Content-Type", "Authorization"],
-      credentials: true, // Allow credentials (cookies)
-    })
-  );
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Be cautious in production, use specific domains instead of '*'
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true, // Allow credentials (cookies)
+  })
+);
 
 app.get("/", (req, res) => {
-    res.send("Hello, World!");
-  });
+  res.send("Hello, World!");
+});
 
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
+app.get("/users", async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
