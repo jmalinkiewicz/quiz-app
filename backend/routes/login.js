3c1ac84e14393901545022bcb71edbe3f443147a
Jakub Malinkiewicz <me@jmalinkiewicz.pl>
2024-07-12 17:20:19 +0200

    create login route with JWT

---
+++ backend/routes/login.js
@@ -0,0 +1,63 @@
+require("dotenv").config();
+
+const { PrismaClient } = require("@prisma/client");
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const prisma = new PrismaClient();
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  const { email, password } = req.body;
+
+  let queriedUser;
+
+  try {
+    user = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    if (user !== null) {
+      queriedUser = user;
+    } else {
+      res.status(401).json({
+        error: "Invalid email or password.",
+      });
+      return;
+    }
+  } catch (e) {
+    console.log(e);
+  }
+
+  if ((await bcrypt.compare(password, queriedUser.password)) === true) {
+    const token = jwt.sign(
+      {
+        id: queriedUser.id,
+      },
+      process.env.SECRET_KEY,
+      {
+        expiresIn: "4h",
+      }
+    );
+
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        secure: true,
+        path: "/",
+        expires: new Date(new Date().getTime() + 4 * 3600000),
+      })
+      .json({
+        message: "Logged in successfully.",
+      });
+  } else {
+    res.status(401).json({
+      error: "Invalid email or password.",
+    });
+  }
+});
+
+exports.logInRouter = router;
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { PrismaClient } = require("@prisma/client");
 const { signUpRouter } = require("./routes/signup");
+const { logInRouter } = require("./routes/login");
 
 const app = express();
 const port = 8000;
@@ -40,6 +41,7 @@ app.get("/users", async (req, res) => {
 });
 
 app.use("/signup", signUpRouter);
+app.use("/login", logInRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
