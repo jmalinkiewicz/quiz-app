c5bf5b70a5da0c5c54f06b387706a1d476840d48
Jakub Malinkiewicz <me@jmalinkiewicz.pl>
2024-07-17 21:19:47 +0200

    create route for fetching all quiz questions and answers

--- backend/routes/quiz.js
+++ backend/routes/quiz.js
@@ -39,4 +39,63 @@ router.get("/available", authenticate, async (req, res) => {
   }
 });
 
+router.get("/start/:quizId", authenticate, async (req, res) => {
+  try {
+    const { quizId } = req.params;
+    const { userId } = req.body;
+
+    const quiz = await prisma.quiz.findUnique({
+      where: {
+        id: quizId,
+      },
+      select: {
+        id: true,
+        title: true,
+        description: true,
+        background: true,
+        questions: {
+          select: {
+            id: true,
+            text: true,
+            answers: {
+              select: {
+                id: true,
+                text: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    if (quiz === null) {
+      res.status(404).json({
+        error: "Quiz not found.",
+      });
+      return;
+    }
+
+    const invite = await prisma.invite.findFirst({
+      where: {
+        userId,
+        quizId,
+      },
+    });
+
+    if (invite === null) {
+      res.status(403).json({
+        error: "Invite not found.",
+      });
+      return;
+    }
+
+    res.json(quiz);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      error: "Server error. Please try again.",
+    });
+  }
+});
+
 exports.quizRouter = router;
